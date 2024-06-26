be03a4b52f278693d8ee66dfdf0753742dad92ca
Aerosmitten <139496075+Aerosmitten@users.noreply.github.com>
2024-06-26 15:39:27 -0600

    Google Search Fry Bread Test Finished

--- tests/googleSearch.test.ts
+++ tests/googleSearch.test.ts
@@ -10,18 +10,19 @@ test("do a search", async () => {
     //search for Fry Bread
     await google.search('fry bread')
     //Get search results
-    let test = await google.getResults()
+    let text = await google.getResults()
     //be sure the search contains the right search
-    expect(Text).toContain('fry bread')
+    expect(text).toContain('fry bread')
+   
     //take screenshot
-    // await getSelection.writeFile(`${__dirname}/google.png`,
-    // await google.driver.takeScreenshot(), "base64",
-    // async (e) => {
-    //     if (e) console.error(e)
-    //     else console.log('save succesful') 
-    // });
-    //Save the text resu;ts
-    fs.writeFile(`${__dirname}/text.txt`, Text, (e) => {
+    await fs.writeFile(`${__dirname}/googleSearchFryBread.png`,
+    await google.driver.takeScreenshot(), "base64",
+    async (e) => {
+        if (e) console.error(e)
+        else console.log('save succesful') 
+    });
+    // Save the text resu;ts
+    fs.writeFile(`${__dirname}/textOfGoogleSearchFryBread.txt`, text, (e) => {
         if (e) console.error(e)
         else console.log("this save succesful too")
     });
