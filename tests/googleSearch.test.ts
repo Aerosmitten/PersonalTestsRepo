3da329748937fade5f1473dc5722aad30df137eb
Aerosmitten <139496075+Aerosmitten@users.noreply.github.com>
2024-06-26 16:06:40 -0600

    One more update to the Google Search Fry Bread Test

--- tests/googleSearch.test.ts
+++ tests/googleSearch.test.ts
@@ -3,18 +3,26 @@ import { Driver } from 'selenium-webdriver/chrome'
 const fs = require('fs')
 const google = new Google()
 
+// test goes here
 test("do a search", async () => {
+    //navigate to Google
     await google.navigate()
+    //search for Fry Bread
     await google.search('fry bread')
-    let test = await google.getResults()
-    expect(Text).toContain('fry bread')
-    await fs.writeFile(`${__dirname}/google.png`,
+    //Get search results
+    let text = await google.getResults()
+    //be sure the search contains the right search
+    expect(text).toContain('fry bread')
+   
+    //take screenshot
+    await fs.writeFile(`${__dirname}/googleSearchFryBread.png`,
     await google.driver.takeScreenshot(), "base64",
     async (e) => {
         if (e) console.error(e)
         else console.log('save succesful') 
     });
-    fs.writeFile(`${__dirname}/text.txt`, Text, (e) => {
+    // Save the text resu;ts
+    fs.writeFile(`${__dirname}/textOfGoogleSearchFryBread.txt`, text, (e) => {
         if (e) console.error(e)
         else console.log("this save succesful too")
     });
