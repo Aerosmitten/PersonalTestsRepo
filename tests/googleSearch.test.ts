4e5324524001dc77e061c129c98c9e1c8c1bdb39
Aerosmitten <139496075+Aerosmitten@users.noreply.github.com>
2024-06-26 14:43:42 -0600

    Started GoogleSearch Test

--- tests/googleSearch.test.ts
+++ tests/googleSearch.test.ts
@@ -3,17 +3,24 @@ import { Driver } from 'selenium-webdriver/chrome'
 const fs = require('fs')
 const google = new Google()
 
+// test goes here
 test("do a search", async () => {
+    //navigate to Google
     await google.navigate()
+    //search for Fry Bread
     await google.search('fry bread')
+    //Get search results
     let test = await google.getResults()
+    //be sure the search contains the right search
     expect(Text).toContain('fry bread')
-    await getSelection.writeFile(`${__dirname}/google.png`,
-    await google.driver.takeScreenshot(), "base64",
-    async (e) => {
-        if (e) console.error(e)
-        else console.log('save succesful') 
-    });
+    //take screenshot
+    // await getSelection.writeFile(`${__dirname}/google.png`,
+    // await google.driver.takeScreenshot(), "base64",
+    // async (e) => {
+    //     if (e) console.error(e)
+    //     else console.log('save succesful') 
+    // });
+    //Save the text resu;ts
     fs.writeFile(`${__dirname}/text.txt`, Text, (e) => {
         if (e) console.error(e)
         else console.log("this save succesful too")
